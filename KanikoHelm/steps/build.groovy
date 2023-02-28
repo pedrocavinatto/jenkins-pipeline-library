3638a2a1dd33b9d75cf55d097e788de533afd234
pedro-easyit <pedro@easyitcloud.com.br>
2023-02-28 11:49:33 -0300

    fix additional repo merge

--- KanikoHelm/steps/build.groovy
+++ KanikoHelm/steps/build.groovy
@@ -5,10 +5,12 @@ void call(){
 
     // Git stuff
     def repo = config.git_repo
-    def additionalRepo = config.git_additional_repo
-    def additionalBranch = config.git_additional_branch
     def branch = config.git_branch
     def chartFolder = config.chartFolder
+    def additionalRepo = config.git_additional_repo
+    def additionalBranch = config.git_additional_branch
+    def gitName = config.git_name
+    def gitEmail = config.git_email
 
     // Docker Hub stuff
     def dockerHubUser = config.dockerHubUser
@@ -52,8 +54,13 @@ void call(){
         node(labelKaniko) {
             stage('Build Kaniko') {
                 git url: repo, branch: branch
-                if (additionalRepo != null && additionalBranch != null) {
-                    git url: additionalRepo, branch: additionalBranch
+                if (additionalRepo != null && additionalBranch != null && gitName != null && gitEmail != null) {
+                    sh """git checkout -b ${labelKaniko}"""
+                    sh """git config --global user.email ${gitEmail} """
+                    sh """git config --global user.name ${gitName} """
+                    sh """git remote add additional-project ${additionalRepo}"""
+                    sh """git fetch additional-project --tags"""
+                    sh """git merge --allow-unrelated-histories additional-project/${additionalBranch}"""
                 }
                 container(name: 'kaniko', shell: '/busybox/sh') {
                     sh """#!/busybox/sh
@@ -84,8 +91,13 @@ void call(){
         node(labelHelm) {
             stage('Deploy project with Helm') {
                 git url: repo, branch: branch
-                if (additionalRepo != null && additionalBranch != null) {
-                    git url: additionalRepo, branch: additionalBranch
+                if (additionalRepo != null && additionalBranch != null && gitName != null && gitEmail != null) {
+                    sh """git checkout -b ${labelKaniko}"""
+                    sh """git config --global user.email ${gitEmail} """
+                    sh """git config --global user.name ${gitName} """
+                    sh """git remote add additional-project ${additionalRepo}"""
+                    sh """git fetch additional-project --tags"""
+                    sh """git merge --allow-unrelated-histories additional-project/${additionalBranch}"""
                 }
                 container(name: 'helm', shell: '/bin/ash') {
                     sh "helm upgrade --install ${tenantID} ./${chartFolder} --set image.tag=${tagVersion} --set image.repository=${dockerHubUser}/${tenantID}"
